12b046c516312780d38ec28ab9ce48bf620d4420
tle83 <tnle833@gmail.com>
2018-11-17 22:51:30 -0600

    Initial upload
    +index page
    +home page
    +pricing xml
    +about page

---
+++ script.js
@@ -0,0 +1,29 @@
+function linkPage(pageName){
+	window.location.assign(pageName);
+}
+
+window.onload = function(){
+	var elements = document.getElementsByClassName('tabLink');
+	for(var i = 0; i < elements.length; i++){
+		elements[i].onclick = function(){
+			document.getElementsById('iframe').src = this.value;
+		}
+	}
+};
+
+//Slide show in home page
+var myIndex = 0;
+carousel();
+
+function carousel(){
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	for(i = 0; i < slides.length; i++){
+		slides[i].style.display = "none";
+	}
+	myIndex++;
+	if(myIndex > slides.length){ myIndex = 1 }
+	slides[myIndex-1].style.display = "block";
+	//change image every 2 seconds
+	setTimeout(carousel, 2000);
+}
